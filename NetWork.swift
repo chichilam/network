27918b373d68ed45cd797a9214c898cf7396cf31
k_rin <k_rin@ap-com.co.jp>
2015-02-03 12:19:47 +0900

    add deletgate method

--- NetWork.swift
+++ NetWork.swift
@@ -8,7 +8,78 @@
 
 import UIKit
 
+protocol NetWorkDelegate : class {
+    func connectionDidFinishLoading(apiName:String!, resutl:AnyObject?, error:NSError?)
+}
+
 class NetWork: NSObject {
+    
+    var receiveData: NSMutableData?
+    var apiName: String?
+    weak var delegate: NetWorkDelegate? = nil
+    
+    /**
+    connet by get using delegate
+    
+    :param: urlStr         url string
+    :param: callingApiName api recognition
+    */
+    func connectByGet(urlStr: String!, callingApiName: String!) {
+        
+        receiveData = NSMutableData()
+        apiName = callingApiName
+        
+        let url = NSURL(string: urlStr.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!
+        
+        var request = NSMutableURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 20.0)
+        
+        request.HTTPShouldHandleCookies = false
+        
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)!
+        
+        connection.start()
+    }
+    
+    /**
+    connet by post using delegate
+    
+    :param: urlStr         url string
+    :param: postStr        body string
+    :param: callingApiName api recognition
+    */
+    func connectByPost(urlStr: String!, bodyStr: String!, callingApiName: String!) {
+        receiveData = NSMutableData()
+        apiName = callingApiName
+        
+        let url = NSURL(string: urlStr.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!
+        
+        let bodyData = bodyStr.dataUsingEncoding(NSUTF8StringEncoding)
+        
+        var request = NSMutableURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 20.0)
+        
+        request.HTTPBody = bodyData
+        request.HTTPShouldHandleCookies = false
+        
+        var connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: true)!
+        
+        connection.start()
+    }
+    
+    func connection(connection: NSURLConnection!, didReceiveData data: NSData!){
+        receiveData?.appendData(data)
+    }
+    
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        delegate?.connectionDidFinishLoading(apiName, resutl: nil, error: error)
+    }
+    
+    func connectionDidFinishLoading(connection: NSURLConnection!) {
+        var error: NSError? = nil
+        
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(receiveData!, options: NSJSONReadingOptions.MutableContainers, error: &error) as NSDictionary
+        
+        delegate?.connectionDidFinishLoading(apiName, resutl: jsonResult, error: error)
+    }
     
     /**
     connect by get
@@ -16,22 +87,24 @@ class NetWork: NSObject {
     :param: apiUrlStr url
     :param: callback  result
     */
-    class func connetByGet(urlStr: String!, callback: (AnyObject, NSError!) -> ()) {
+    class func connetByGet(urlStr: String!, callback: (AnyObject?, NSError?) -> ()) {
         
-        let url:NSURL = NSURL(string: urlStr)!
+        let url:NSURL = NSURL(string: urlStr.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!
                 
-        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 20.0)
         
-        request.timeoutInterval = 20
         request.HTTPShouldHandleCookies = false
         
         let queue:NSOperationQueue = NSOperationQueue()
         
         NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response:NSURLResponse!, data:NSData!, error:NSError!) -> Void in
             if(error == nil) {
-                var jsonResutl: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
                 
-                callback(jsonResutl,nil)
+                var jsonError : NSError?
+                
+                var jsonResutl: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
+                
+                callback(jsonResutl,jsonError)
             } else {
                callback(data,error)
             }
@@ -45,14 +118,13 @@ class NetWork: NSObject {
     :param: bodyStr   post data
     :param: callback  resutl
     */
-    class func connectByPost(urlStr: String!, bodyStr: String!, callback:(AnyObject, NSError!) -> ()) {
+    class func connectByPost(urlStr: String!, bodyStr: String!, callback:(AnyObject?, NSError?) -> ()) {
         let url:NSURL = NSURL(string: urlStr)!
         
-        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 20.0)
         
         let bodyData = bodyStr.dataUsingEncoding(NSUTF8StringEncoding)
         
-        request.timeoutInterval = 20
         request.HTTPBody = bodyData
         request.HTTPShouldHandleCookies = false
         
@@ -60,9 +132,12 @@ class NetWork: NSObject {
         
         NSURLConnection.sendAsynchronousRequest(request, queue: queue) { (response:NSURLResponse!, data:NSData!, error:NSError!) -> Void in
             if(error == nil) {
-                var jsonResutl: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
                 
-                callback(jsonResutl,nil)
+                var jsonError : NSError?
+                
+                var jsonResutl: NSDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
+                
+                callback(jsonResutl,jsonError)
             } else {
                 callback(data,error)
             }
@@ -75,20 +150,20 @@ class NetWork: NSObject {
     :param: apiUrlStr url
     :param: callback  result
     */
-    class func connectSessionByGet(urlStr: String!, callback: (AnyObject, NSError!) -> ()) {
+    class func connectSessionByGet(urlStr: String!, callback: (AnyObject?, NSError?) -> ()) {
         
-        let url:NSURL = NSURL(string: urlStr)!
+        let url:NSURL = NSURL(string: urlStr.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!)!
         
         var task = NSURLSession.sharedSession().dataTaskWithURL(url, completionHandler:{data, response, error in
             
             if(error == nil) {
                 
+                var jsonError : NSError?
+                
                 //convert json data to dictionary
-                var dict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                var jsonResutl = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
                 
-                println(dict)
-                
-                callback(dict, nil)
+                callback(jsonResutl, jsonError)
                 
             } else {
                 callback(data, error)
@@ -98,14 +173,13 @@ class NetWork: NSObject {
         task.resume()
     }
     
-    class func connectSessionByPost(urlStr: String!, bodyStr: String!, callback: (AnyObject, NSError!) -> ()) {
+    class func connectSessionByPost(urlStr: String!, bodyStr: String!, callback: (AnyObject?, NSError?) -> ()) {
         let url:NSURL = NSURL(string: urlStr)!
         
-        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url)
+        var request: NSMutableURLRequest = NSMutableURLRequest(URL: url, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 20.0)
         
         let bodyData = bodyStr.dataUsingEncoding(NSUTF8StringEncoding)
         
-        request.timeoutInterval = 20
         request.HTTPBody = bodyData
         request.HTTPShouldHandleCookies = false
         
@@ -113,12 +187,12 @@ class NetWork: NSObject {
             
             if(error == nil) {
                 
+                var jsonError : NSError?
+                
                 //convert json data to dictionary
-                var dict = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+                var jsonResutl = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
                 
-                println(dict)
-                
-                callback(dict, nil)
+                callback(jsonResutl, jsonError)
                 
             } else {
                 callback(data, error)
